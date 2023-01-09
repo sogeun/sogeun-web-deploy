d25971c0881d527411d9de59914a938d5adaa8e4
jongho <jj0101065@gmail.com>
2023-01-09 23:21:37 +0900

    feat : 회원가입 작업중

--- components/TextField/index.tsx
+++ components/TextField/index.tsx
@@ -7,11 +7,11 @@ interface Props {
   onChange?: () => {};
 }
 
-const TextField = ({ label, placeholder, onChange }: Props) => {
+const TextField = ({ label, placeholder, onChange, ...rest }: Props) => {
   return (
-    <Container>
-      <Label>{label}</Label>
-      <EleInput onChange={onChange} placeholder={placeholder} />
+    <Container label={label}>
+      {label && <Label>{label}</Label>}
+      <EleInput {...rest} onChange={onChange} placeholder={placeholder} />
     </Container>
   );
 };
@@ -23,28 +23,21 @@ const Label = styled.label`
   padding: 0;
   margin-left: 5px;
 
-  width: 35px;
+  width: 100%;
   height: 22px;
 
-  /* Inside auto layout */
   flex: none;
   order: 0;
   flex-grow: 0;
 
-  font-family: "Pretendard";
+  font-family: "Pretendard", "serif";
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 22px;
-  /* identical to box height, or 157% */
-
   letter-spacing: -0.01em;
 
-  /* white */
-
   color: #ffffff;
-
-  /* Inside auto layout */
 
   flex: none;
   flex-grow: 0;
@@ -52,8 +45,6 @@ const Label = styled.label`
 
 const EleInput = styled.input`
   box-sizing: border-box;
-
-  /* Auto layout */
 
   display: flex;
   flex-direction: row;
@@ -64,29 +55,27 @@ const EleInput = styled.input`
   width: 333px;
   height: 52px;
 
-  /* Gray_03 */
-
   border: 1px solid #7f848b;
   border-radius: 8px;
-
-  /* Inside auto layout */
 
   flex: none;
   order: 1;
   flex-grow: 0;
+
+  background-color: transparent;
+
+  font-family: "Pretendard", "serif";
+  color: white;
 `;
 
-const Container = styled.div`
+const Container = styled.div<{ label?: string }>`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding: 0px;
+  padding: 0;
   gap: 4px;
 
   width: 333px;
-  height: 78px;
-
-  /* Inside auto layout */
 
   flex: none;
   order: 0;
--- pages/signup/[id].tsx
+++ pages/signup/[id].tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Button from "~/components/Button";
 import { ParsedStorage } from "~/utils/storage";
 import routes from "~/constants/routes";
+import TextField from "~/components/TextField";
 
 const SignUp = () => {
   const router = useRouter();
@@ -25,17 +26,137 @@ const SignUp = () => {
 
   return (
     <Container>
-      <div>{matchedItem?.content}</div>
+      {matchedItem?.header}
+      {matchedItem?.content}
       <ButtonWrap>
-        <Button title={"다음"} hasNextButton onClick={handleNext} />
+        <Button
+          title={matchedItem?.buttonTitle}
+          hasNextButton
+          onClick={handleNext}
+        />
       </ButtonWrap>
     </Container>
   );
 };
 
+const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 100%;
+`;
+
+const SignUpHeader = styled.div`
+  position: absolute;
+  width: 375px;
+  height: 56px;
+  top: 50px;
+
+  font-family: "Pretendard", serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+const Agree = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  font-family: "Pretendard", serif;
+
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+
+  align-items: center;
+  letter-spacing: -0.04em;
+
+  color: #ffffff;
+
+  .content {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    /* identical to box height, or 157% */
+
+    display: flex;
+    align-items: center;
+    letter-spacing: -0.01em;
+    text-decoration-line: underline;
+    color: #7f848b;
+  }
+`;
+
+const ServiceAgree = styled.div`
+  width: 334.15px;
+  height: 193.7px;
+
+  border-radius: 5px;
+
+  background: #e4eaf2;
+  overflow-y: auto;
+
+  font-family: "Pretendard", serif;
+  font-size: 12px;
+  line-height: 18px;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.01em;
+
+  color: #585c62;
+`;
+
 const signupList = [
-  { id: 1, content: 1 },
-  { id: 2, content: 2 },
+  {
+    id: 1,
+    header: <SignUpHeader>회원가입하기</SignUpHeader>,
+    content: (
+      <FieldContainer>
+        <TextField label={"이메일"} placeholder={"예) user@sample.com"} />
+        <>
+          <TextField label={"비밀번호"} placeholder={"비밀번호"} />
+          <TextField placeholder={"비밀번호 확인"} />
+        </>
+        <TextField
+          label={"휴대폰번호"}
+          placeholder={"휴대폰 번호를 입력해주세요."}
+        />
+        <TextField label={"닉네임"} placeholder={"닉네임을 입력해주세요"} />
+      </FieldContainer>
+    ),
+    buttonTitle: "다음으로 넘어갈래요.",
+  },
+  {
+    id: 2,
+    header: <SignUpHeader>약관동의</SignUpHeader>,
+    content: (
+      <div>
+        <Agree>
+          서비스 이용약관 동의
+          <div className={"content"}>내용보기</div>
+        </Agree>
+        <ServiceAgree>
+          Lorem ipsum dolor sit amet consectetur. Turpis in eu aliquam mi. Velit
+          diam sed id imperdiet mauris amet non turpis. Id euismod adipiscing et
+          cras. Sapien ege tellus nibh adipiscing et cras. Sapien egestas
+        </ServiceAgree>
+        <Agree>
+          개인정보 수집 및 이용 동의
+          <div className={"content"}>내용보기</div>
+        </Agree>
+        <ServiceAgree>
+          Lorem ipsum dolor sit amet consectetur. Turpis in eu aliquam mi. Velit
+          diam sed id imperdiet mauris amet non turpis. Id euismod adipiscing et
+          cras. Sapien ege tellus nibh adipiscing et cras. Sapien egestas
+        </ServiceAgree>
+      </div>
+    ),
+    buttonTitle: "동의할게요.",
+  },
 ];
 
 const Container = styled.main`
@@ -43,7 +164,8 @@ const Container = styled.main`
   flex-direction: column;
   justify-content: space-between;
   padding: 14rem 2rem 4.2rem;
-  height: 100%;
+  height: 100vh;
+  margin-bottom: 20px;
 `;
 
 const ButtonWrap = styled.div`
@@ -51,6 +173,7 @@ const ButtonWrap = styled.div`
   flex-direction: column;
   width: 100%;
   gap: 2.4rem;
+  margin-top: 40px;
 `;
 
 export default SignUp;
